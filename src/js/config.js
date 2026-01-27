e87ada072c5f5bfd6eb70567a933e686ec682ff7
john adeoye <adoyejohn15@gmail.com>
2026-01-27 11:15:21 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 20;
+export const RES_PER_PAGE = 10;
+export const KEY = '7b96b8ff-d75e-4bd2-b411-3eb23b38eb98';
+export const MODAL_CLOSE_SEC = 2.5;
